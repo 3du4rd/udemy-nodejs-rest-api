ee3a08fdd4fa1e9461862710751efeaf59596c14
3du4rd <3du4rd@gmail.com>
2021-08-12 14:31:40 -0500

    feat: :sparkles: 365. REST APIs, Clients & CORS Errors

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // app.use(express.urlencoded()); // x-www-form-urlencoded <form>
 app.use(express.json()); // application/json
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
