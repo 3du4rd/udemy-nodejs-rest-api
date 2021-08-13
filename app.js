fb40ebf1959275c43a38357e9c9fe3da9b91596d
3du4rd <3du4rd@gmail.com>
2021-08-12 19:29:47 -0500

    feat: :sparkles: 378. Static Images & Error Handling

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use('/images', express.static(path.join(__dirname,'images')));
 
 // app.use(express.urlencoded()); // x-www-form-urlencoded <form>
 app.use(express.json()); // application/json
 
+//-> Middleware para el Manejo de CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
@@ -21,6 +23,14 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+//-> Middleware para Manejo de Errores
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message });
+  });
 
 mongoConnect
 .then(result => {
