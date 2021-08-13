2b867299a9ede0b42bdaf303f46c3d1779a39ead
3du4rd <3du4rd@gmail.com>
2021-08-12 23:00:09 -0500

    feat: :sparkles: 392. Adding Auth Middleware to All Routes

--- routes/feed.js
+++ routes/feed.js
@@ -12,6 +12,7 @@ router.get('/posts', isAuth, feedController.getPosts);
 // POST /feed/post
 router.post(
     '/post',
+    isAuth,
     [
       body('title')
         .trim()
@@ -23,10 +24,10 @@ router.post(
     feedController.createPost
   );
 
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
 
 router.put(
-    '/post/:postId',
+    '/post/:postId', isAuth,
     [
       body('title')
         .trim()
@@ -38,6 +39,6 @@ router.put(
     feedController.updatePost
   );
 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
