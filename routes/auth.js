2db3724e27fefe054407c3217d58a346715c3ecf
3du4rd <3du4rd@gmail.com>
2021-08-12 22:04:32 -0500

    feat: :sparkles: 387. Signing Users Up

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,35 @@ const express = require('express');
 const { body } = require('express-validator');
 
 const User = require('../models/user');
-//const authController = require('../controllers/auth');
+const authController = require('../controllers/auth');
 
 const router = express.Router();
 
 router.put(
-  '/signup'
+  '/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject('E-Mail address already exists!');
+          }
+        });
+      })
+      .normalizeEmail(),
+    body('password')
+      .trim()
+      .isLength({ min: 5 }),
+    body('name')
+      .trim()
+      .not()
+      .isEmpty()
+  ],
+  authController.signup
 );
 
-//router.post('/login', authController.login);
+router.post('/login', authController.login);
 
 module.exports = router;
