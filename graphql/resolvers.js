4e0a6d9ef33f990f9eeb74b185a8d550ac2822eb
3du4rd <3du4rd@gmail.com>
2021-08-15 15:16:58 -0500

    feat: :sparkles: 424. Adding Input Validation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,7 @@
 
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
+
 const User = require('../models/user');
 
 module.exports = {
@@ -10,9 +12,26 @@ module.exports = {
       views: 8888
     };
   },
-  
+
   createUser: async function({ userInput }, req) {
     //   const email = args.userInput.email;
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: 'E-Mail is invalid.' });
+    }
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: 'Password too short!' });
+    }
+    if (errors.length > 0) {
+      const error = new Error('Invalid input.');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+    
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
       const error = new Error('User exists already!');
