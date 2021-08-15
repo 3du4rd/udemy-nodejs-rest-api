6e968b79692b3a8c296f0fd506f3a3dd9b5e1afa
3du4rd <3du4rd@gmail.com>
2021-08-15 12:10:09 -0500

    feat: :sparkles: 415. Sorting Correctly

--- controllers/feed.js
+++ controllers/feed.js
@@ -12,7 +12,9 @@ exports.getPosts = async (req, res, next) => {
   let totalItems;
   try{
     const totalItems = await Post.find().countDocuments();
-    const posts = await Post.find().populate('creator')
+    const posts = await Post.find()
+          .populate('creator')
+          .sort({ createdAt: -1 })
           .skip((currentPage - 1) * perPage)
           .limit(perPage);
     res.status(200)
