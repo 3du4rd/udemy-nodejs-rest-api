e417de297bd30ec58f5985cf7c5ed6571c3569df
3du4rd <3du4rd@gmail.com>
2021-08-15 15:37:09 -0500

    feat: :sparkles: 426. Connecting the Frontend to the GraphQL

--- app.js
+++ app.js
@@ -51,6 +51,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
 
