165d790850a1481e5626ec087fdb0f38f2939b27
3du4rd <3du4rd@gmail.com>
2021-08-16 12:21:34 -0500

    feat: :sparkles: 442. Managing the User Status

--- graphql/schema.js
+++ graphql/schema.js
@@ -53,6 +53,7 @@ module.exports = buildSchema(`
         login(email: String!, password: String!): AuthData!
         posts(page: Int): PostData!
         post(id: ID!): Post!
+        user: User!
     }
 
     type RootMutation {
@@ -60,6 +61,7 @@ module.exports = buildSchema(`
         createPost(postInput: PostInputData): Post!
         updatePost(id: ID!, postInput: PostInputData): Post!
         deletePost(id: ID!): Boolean
+        updateStatus(status: String!): User!
     }
 
     schema {
