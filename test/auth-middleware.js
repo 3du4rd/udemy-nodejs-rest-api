59657960da5c35e057e2717f9f39f2cde22b2564
3du4rd <3du4rd@gmail.com>
2021-08-17 00:23:01 -0500

    test: :white_check_mark: 467. Testing the Auth Middleware

---
+++ test/auth-middleware.js
@@ -0,0 +1,14 @@
+const expect = require('chai').expect;
+
+const authMiddleware = require('../middleware/is-auth');
+
+it('should throw an error if no authorization header is present', function() {
+  const req = {
+    get: function(headerName) {
+      return null;
+    }
+  };
+  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+    'Not authenticated.'
+  );
+});
