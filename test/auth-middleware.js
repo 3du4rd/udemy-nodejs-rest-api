947d4205b7b57b29602daeb55c193a80f07f3830
3du4rd <3du4rd@gmail.com>
2021-08-17 00:27:14 -0500

    test: :white_check_mark: 468. Organizing Multiple Tests

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,13 +2,25 @@ const expect = require('chai').expect;
 
 const authMiddleware = require('../middleware/is-auth');
 
-it('should throw an error if no authorization header is present', function() {
-  const req = {
-    get: function(headerName) {
-      return null;
-    }
-  };
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-    'Not authenticated.'
-  );
-});
+//-> describe permite agrupar test cases
+describe('Auth middleware', function() {
+    it('should throw an error if no authorization header is present', function() {
+      const req = {
+        get: function(headerName) {
+          return null;
+        }
+      };
+      expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+        'Not authenticated.'
+      );
+    });
+  
+    it('should throw an error if the authorization header is only one string', function() {
+      const req = {
+        get: function(headerName) {
+          return 'xyz';
+        }
+      };
+      expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+    });
+  });
